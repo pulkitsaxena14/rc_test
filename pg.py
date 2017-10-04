5c55e778f94b99bd76af1a7ba9942f5d3c19f4d6
pulkitsaxena14 <pulkitsaxena14@gmail.com>
2017-10-04 19:07:26 +0530

    Added Test Case to pg.py

--- pg.py
+++ pg.py
@@ -1,8 +1,23 @@
 from sqlalchemy import create_engine
 import pandas as pd
 
-engine = create_engine('postgresql://test_user:testtest@localhost/redcarpet')
-c = engine.connect()
-df1 = pd.read_csv('/home/saxena/PycharmProjects/test1/Tests/Zeta_Wallet_Loads_Zeta_Loads.csv')
-df1.to_sql('zeta_wallet', engine, if_exists='append', index=False)
-c.close()
+def connect_pg():
+    engine = create_engine('postgresql://test_user:testtest@localhost/redcarpet')
+    return engine
+
+def create_db(location_csv):
+    eng = connect_pg()
+    c = eng.connect()
+    df1 = pd.read_csv(location_csv)
+    df1.to_sql('zeta_wallet', eng, if_exists='append', index=False)
+    c.close()
+
+def test_create_db():
+    loc = '/home/saxena/PycharmProjects/test1/Tests/Zeta_Wallet_Loads_Zeta_Loads.csv'
+    create_db(loc)
+    eng = connect_pg()
+    c = eng.connect()
+    df1 = pd.read_sql_table('zeta_wallet', eng)
+    c.close()
+    df2 = pd.read_csv(loc)
+    assert (df1.head()).equals(df2.head())
