a6666fccb27624c7d3e8e1455e0e43f22067bec6
pulkitsaxena14 <pulkitsaxena14@gmail.com>
2017-10-04 18:30:50 +0530

    Fixed bug

--- pg_data_check.py
+++ pg_data_check.py
@@ -4,14 +4,17 @@ import pandas as pd
 engine = create_engine('postgresql://test_user:testtest@localhost/redcarpet')
 c = engine.connect()
 df1 = pd.read_csv('/home/saxena/PycharmProjects/test1/Tests/Zeta_Wallet_Loads_Zeta_Loads.csv')
-new_index = 0
-if engine.has_table('zeta_wallet') is True:
+
+if engine.has_table('zeta_wallet') is False:
+    df1.index += 1
+    df1.to_sql('zeta_wallet', engine, if_exists='append', index=True)
+    engine.execute("ALTER TABLE zeta_wallet ADD PRIMARY KEY(index)")
+else:
     res = engine.execute('Select count(*) as cnt from zeta_wallet;')
     for i in res:
         df1.index += (i['cnt'] + 1)
+    df1.to_sql('zeta_wallet', engine, if_exists='append', index=True)
 
-df1.to_sql('zeta_wallet', engine, if_exists='append', index=True)
-print(df1)
 df2 = pd.read_sql_table('zeta_wallet', engine)
 df1.fillna(value='None', inplace=True)
 df2.fillna(value='None', inplace=True)
