01d1e47439fdca388200119912865e99365f4ba2
pulkitsaxena14 <pulkitsaxena14@gmail.com>
2017-10-05 15:03:29 +0530

    Update

--- add_df_to_db.py
+++ add_df_to_db.py
@@ -9,7 +9,7 @@ meta = MetaData(bind=engine)
 connection = engine.connect()
 zeta_wallet = Table('zeta_wallet', meta,
                     Column('wallet_id', INTEGER, Sequence('wallet_id_seq'), primary_key=True),
-                    Column('Date', TEXT, nullable=True),
+                    Column('DDate', TEXT, nullable=True),
                     Column('From_to_party', TEXT, nullable=True),
                     Column('Credit', TEXT, nullable=True),
                     Column('Debit', TEXT, nullable=True),
@@ -43,5 +43,5 @@ if engine.has_table('zeta_wallet') is False:
 ins = zeta_wallet.insert()
 df1 = pd.read_csv('/home/saxena/PycharmProjects/test1/Tests/Zeta_Wallet_Loads_Zeta_Loads.csv')
 for tup in df1.itertuples():
-    engine.execute(zeta_wallet.insert(), Date=tup[1], From_to_party = tup[2], Credit = tup[3], Debit = tup[4], Balance = tup[5], Bank_ref_txn_id = tup[6], Load = tup[7], Key = tup[8], Lender = tup[9], Zeta_Order_Id = tup[10], DMI_User_bal = tup[11], RC_User_bal = tup[12], DMI_usr_bal_post_may = tup[13], Chandan_comment = tup[14], Current_date = tup[15], Status = tup[16], Unnamed_col1 = tup[17], Unnamed_col2 = tup[18], Unnamed_col3 = tup[19], Unnamed_col4 = tup[20], Unnamed_col5 = tup[21], Unnamed_col6 = tup[22], Unnamed_col7 = tup[23], Unnamed_col8 = tup[24], Unnamed_col9 = tup[25], Actual_bal = tup[26])
+    engine.execute(zeta_wallet.insert(), DDate=tup[1], From_to_party = tup[2], Credit = tup[3], Debit = tup[4], Balance = tup[5], Bank_ref_txn_id = tup[6], Load = tup[7], Key = tup[8], Lender = tup[9], Zeta_Order_Id = tup[10], DMI_User_bal = tup[11], RC_User_bal = tup[12], DMI_usr_bal_post_may = tup[13], Chandan_comment = tup[14], Current_date = tup[15], Status = tup[16], Unnamed_col1 = tup[17], Unnamed_col2 = tup[18], Unnamed_col3 = tup[19], Unnamed_col4 = tup[20], Unnamed_col5 = tup[21], Unnamed_col6 = tup[22], Unnamed_col7 = tup[23], Unnamed_col8 = tup[24], Unnamed_col9 = tup[25], Actual_bal = tup[26])
 connection.close()
